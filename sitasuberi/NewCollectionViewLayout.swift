be7642f002eb93f9c618801545d75490866cab6a
Yo Higashida <penguin4731@gmail.com>
2021-11-26 19:33:28 +0900

    横スクロール実装

--- sitasuberi/NewCollectionViewLayout.swift
+++ sitasuberi/NewCollectionViewLayout.swift
@@ -1,40 +1,47 @@
 import UIKit
- 
-class NewCollectionViewLayout: UICollectionViewLayout {
-    
-    
-    //行数
-    let columns = 8
-    let rows = 16
-    //レイアウト
-    private var layoutData = [UICollectionViewLayoutAttributes]()
-    var contentHeight: CGFloat = 0
-    
-    var contentWidth: CGFloat {
-            guard let collectionView = collectionView else { return 0 }
-            return collectionView.bounds.width
-        }
-    
-    //func layoutDate;.scrollDirection = .horizontal() // 横スクロール
 
-    override var collectionViewContentSize: CGSize {
-        return CGSize(width: contentWidth, height: collectionView!.bounds.height)
+class NewCollectionViewLayout: UICollectionViewLayout {
+    // セルのサイズ
+    let cellSize: CGFloat = 50
+    var cellCount: Int {
+        guard let collectionView = collectionView else { return 0 }
+        return collectionView.visibleCells.count
     }
-    //レイアウトの準備
+    
+    // 行数
+    var rows: Int {
+        guard let collectionView = collectionView else { return 0 }
+        let allHeight = collectionView.bounds.height - 100
+        return Int(allHeight / cellSize)
+    }
+    var columns: Int {
+        guard let collectionView = collectionView else { return 0 }
+        print(collectionView.numberOfItems(inSection: 0))
+        return collectionView.numberOfItems(inSection: 0) / rows
+    }
+    
+    // レイアウト
+    private var layoutData = [UICollectionViewLayoutAttributes]()
+    var contentWidth: CGFloat {
+        return CGFloat(columns) * cellSize
+    }
+    var contentHeight: CGFloat {
+        return CGFloat(rows) * cellSize
+    }
+    
+    override var collectionViewContentSize: CGSize {
+        return CGSize(width: contentWidth, height: contentHeight)
+    }
+    // レイアウトの準備
     override func prepare() {
         super.prepare()
         self.layoutData.removeAll()
-        let allWidth  = collectionView!.bounds.width
-        let columnWidth = allWidth / CGFloat(self.columns)
-        let columnHeight = columnWidth
-//        var x:CGFloat = 0
-        var x:CGFloat = columnHeight * CGFloat((columns - 1))
+        var x:CGFloat = cellSize * CGFloat((columns - 1))
         var y:CGFloat = 0
-//        scrollDirection = .horizontal
         for count in 0 ... collectionView!.numberOfItems(inSection: 0){
             let indexPath = NSIndexPath(item: count, section: 0)
             //レイアウトの配列に位置とサイズを登録する。
-            let frame = CGRect(x: x, y: y, width: columnWidth, height: columnHeight)
+            let frame = CGRect(x: x, y: y, width: cellSize, height: cellSize)
             
             //ここで左右(y)だけの余白を設定してるよ
             let insetFrame = frame.insetBy(dx: 5, dy: 0)
@@ -42,25 +49,23 @@ class NewCollectionViewLayout: UICollectionViewLayout {
             let attributes = UICollectionViewLayoutAttributes(forCellWith: indexPath as IndexPath)
             attributes.frame = insetFrame
             layoutData.append(attributes)
-            //contentHeight = max(contentHeight, frame.maxY)
             
-            
-//            ここでcellをどの場所に配置するかを指定している
-//            これはindexPath[1]が右上が0,左下が160(一番最後のセルのindex)となるように配置している(縦書き配置)
+            // ここでcellをどの場所に配置するかを指定している
+            // これはindexPath[1]が右上が0,左下が160(一番最後のセルのindex)となるように配置している(縦書き配置)
             if ((count + 1) % rows != 0){
-                y += columnHeight
+                y += cellSize
             }else{
                 y = 0
-                x -= columnWidth
+                x -= cellSize
             }
         }
+        let cellHeight = collectionView!.frame.minY - 93
+        collectionView?.setContentOffset(CGPoint(x: 100, y: cellHeight), animated: true)
+        collectionView?.isPagingEnabled = true
     }
-    
-    
     
     //レイアウトを返す
     override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
         return layoutData
     }
-
 }
